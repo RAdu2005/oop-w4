189307ca937eaa5022954c3e37e2d66d7845403d
Radu Stoiana <radu.stoiana@gmail.com>
2024-01-30 15:38:39 +0200

    modified:   src/main/java/main/Calculator.java 	modified:   target/classes/main/Calculator.class

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -9,32 +9,34 @@ public class Calculator {
             sum += i.courseGrade;
         }
 
-        ans = (sum * 1.0) / s.gradeList.size();
+        if(sum != 0) {ans = (sum * 1.0) / s.gradeList.size();}
         return ans;
     }
 
     public static double getMedianGrade(Student s){
         double ans = 0;
 
-        int[] gradeList = new int[1000];
-        int index = 0;
-        for(Grade i : s.gradeList){
-            gradeList[index++] = i.courseGrade;
-        }
-        
-        int aux;
-        for(int i = 0; i < index - 1; i++){
-            for(int j = i + 1; j < index ; j++){
-                if(gradeList[i] > gradeList[j]){
-                    aux = gradeList[i];
-                    gradeList[i] = gradeList[j];
-                    gradeList[j] = aux; 
+        if(s.gradeList.size() != 0){
+            int[] gradeList = new int[1000];
+            int index = 0;
+            for(Grade i : s.gradeList){
+                gradeList[index++] = i.courseGrade;
+            }
+            
+            int aux;
+            for(int i = 0; i < index - 1; i++){
+                for(int j = i + 1; j < index ; j++){
+                    if(gradeList[i] > gradeList[j]){
+                        aux = gradeList[i];
+                        gradeList[i] = gradeList[j];
+                        gradeList[j] = aux; 
+                    }
                 }
             }
-        }
 
-        if(index % 2 == 1) ans = gradeList[index / 2];
-        else ans = (gradeList[index / 2] + gradeList[index / 2 - 1]) / 2.0;   
+            if(index % 2 == 1) ans = gradeList[index / 2];
+            else ans = (gradeList[index / 2] + gradeList[index / 2 - 1]) / 2.0;
+        }
         
         return ans;
     }
